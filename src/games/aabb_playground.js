767296021a2421e63b70ac5083810c34da44dfce
Harrison Shoebridge <harrison@theshoebridges.com>
2017-08-01 22:54:45 -0700

    Add width and height guides

--- src/games/aabb_playground.js
+++ src/games/aabb_playground.js
@@ -4,141 +4,145 @@ class AABBPlayground extends Game {
 
     let mid = new Point(this.r.width/2, this.r.height/2)
 
-    this.anchorA = this.createAnchor(mid.x - 75, mid.y - 125)
-    this.anchorB = this.createAnchor(mid.x + 75, mid.y + 125)
+    this.box = this.createBox()
 
-    this.box = this.createBox(this.anchorA, this.anchorB)
+    this.box.x = mid.x - this.box.size.x/2
+    this.box.y = mid.y - this.box.size.y/2
+
+    this.boxMin = this.createAnchor(mid.x - 75, mid.y - 75, this.box, true)
+    this.boxMax = this.createAnchor(mid.x + 75, mid.y + 75, this.box, false)
+
+    let textSettings = { align: 'center' }
+
+    this.widthText = new PIXI.Text("", textSettings)
+    this.widthText.anchor.x = 0.5
+    this.widthText.anchor.y = 1
+    this.heightText = new PIXI.Text("", textSettings)
+    this.heightText.anchor.x = 0
+    this.heightText.anchor.y = 0.5
+
     this.stage.addChild(this.box)
+    this.stage.addChild(this.boxMin)
+    this.stage.addChild(this.boxMax)
 
-    this.stage.addChild(this.anchorA)
-    this.stage.addChild(this.anchorB)
+    this.stage.addChild(this.widthText)
+    this.stage.addChild(this.heightText)
   }
 
   update(dt) {
     let g = this.box
-    let bb = this.boxBounds()
+
+    let pw = g.width.toFixed(0)
+    let ph = g.height.toFixed(0)
 
     g.clear()
 
-    if (bb.width == 100 && bb.height == 100) {
+    if (pw == 100 && ph == 100) {
       g.beginFill(0x88C8DD)
-    } else if (bb.width == 200 && bb.height == 50) {
+    } else if (pw == 200 && ph == 50) {
       g.beginFill(0xECA5B3)
     } else {
       g.beginFill(0x9EDD88)
     }
 
-    g.drawRect(0, 0, bb.width, bb.height)
+    g.drawRect(0, 0, this.box.size.x, this.box.size.y)
     g.endFill()
 
-    g.x = bb.x
-    g.y = bb.y
+    this.widthText.x = g.x + g.width/2
+    this.widthText.y = g.y
+
+    this.heightText.x = g.x + g.width
+    this.heightText.y = g.y + g.height/2
+
+    this.widthText.text = pw.toString()
+    this.heightText.text = ph.toString()
+
+    this.boxMin.x = g.x
+    this.boxMin.y = g.y
+
+    this.boxMax.x = g.x + this.box.size.x
+    this.boxMax.y = g.y + this.box.size.y
   }
 
-  boxBounds() {
-    let awidth = this.anchorA.width
-    let aheight = this.anchorA.height
-
-    let min = new Point()
-    min.x = this.anchorA.x + awidth/2
-    min.y = this.anchorA.y + aheight/2
-
-    let max = new Point()
-    max.x = this.anchorB.x + awidth/2
-    max.y = this.anchorB.y + aheight/2
-
-    return new AABB(min, max)
-  }
-
-  createBox(anchorA, anchorB) {
-    let b = new PIXI.Graphics()
-    b.interactive = true
-
-    b.
-      on('pointerdown', this.anchorDragStart).
-      on('pointerup', this.anchorDragEnd).
-      on('pointerupoutside', this.anchorDragEnd).
-      on('pointermove', this.boxDragMove)
-
-    b.a1 = anchorA
-    b.a2 = anchorB
-
-    console.log(b.interactive)
-
-    return b
-  }
- 
-  createAnchor(x, y) {
+  createAnchor(x, y, box, upper) {
     let anchor = new PIXI.Sprite(anchorTexture(this.r))
+    anchor.anchor.x = 0.5
+    anchor.anchor.y = 0.5
+
+    anchor.x = x
+    anchor.y = y
+
+    anchor.box = box
+    anchor.isUpper = upper
 
     anchor.interactive = true
 
-    anchor.raw = new Point()
-    
-    anchor.setPos = function(x, y) {
-      console.log(x, y, this)
-      anchor.raw.x = x
-      anchor.raw.y = y
-
-      let s = snapPointToGrid(25, anchor.raw.x, anchor.raw.y)
-
-      anchor.x = s.x
-      anchor.y = s.y
-    }
-
-    anchor.setPos(x, y)
-
     anchor.
-      on('pointerdown', this.anchorDragStart).
-      on('pointerup', this.anchorDragEnd).
-      on('pointerupoutside', this.anchorDragEnd).
+      on('pointerdown', this.dragStart).
+      on('pointerup', this.dragEnd).
+      on('pointerupoutside', this.dragEnd).
       on('pointermove', this.anchorDragMove)
 
     return anchor
+  }
+
+  createBox() {
+    let b = new PIXI.Graphics()
+    b.interactive = true
+
+    b.size = new Point(100, 100)
+
+    b.
+      on('pointerdown', this.dragStart).
+      on('pointerup', this.dragEnd).
+      on('pointerupoutside', this.dragEnd).
+      on('pointermove', this.boxDragMove)
+
+    return b
+  }
+
+  dragStart(e) {
+    this.dragging = true
+
+    this.lastPos = e.data.getLocalPosition(this.parent);
+  }
+
+  dragEnd(e) {
+    this.dragging = false
   }
 
   boxDragMove(ev) {
     if (this.dragging) {
       let raw = ev.data.getLocalPosition(this.parent)
 
-      let diff = new Point(raw.x - this.lastPoint.x, raw.y - this.lastPoint.y)
+      let diff = new Point(raw.x - this.lastPos.x, raw.y - this.lastPos.y)
 
-      this.a1.raw.x += diff.x
-      this.a2.raw.x += diff.x
+      this.x += diff.x
+      this.y += diff.y
 
-      this.a1.x = this.a1.raw.x
-      this.a2.x = this.a2.raw.x
-
-      this.a1.raw.y += diff.y
-      this.a2.raw.y += diff.y
-
-      this.a1.y = this.a1.raw.y
-      this.a2.y = this.a2.raw.y
-
-      this.lastPoint = raw
+      this.lastPos = raw
     }
-  }
-
-  anchorDragStart(ev) {
-    this.dragging = true
-
-    this.alpha = 0.5
-
-    this.lastPoint = ev.data.getLocalPosition(this.parent)
-  }
-
-  anchorDragEnd(ev) {
-    this.dragging = false
-
-    this.alpha = 1
   }
 
   anchorDragMove(ev) {
     if (this.dragging) {
-      let raw = ev.data.getLocalPosition(this.parent);
+      let raw = ev.data.getLocalPosition(this.parent)
 
-      console.log(raw)
-      this.setPos(raw.x, raw.y)
+      let diff = new Point(raw.x - this.lastPos.x, raw.y - this.lastPos.y)
+      if (this.isUpper) {
+        let ogSize = this.box.size
+
+        this.box.size.x -= diff.x
+        this.box.size.y -= diff.y
+
+        this.box.x += diff.x
+        this.box.y += diff.y
+      } else {
+        this.box.size.x += diff.x
+        this.box.size.y += diff.y
+      }
+
+      this.lastPos = raw
     }
   }
 }
