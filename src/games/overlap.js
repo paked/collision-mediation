f1205e9a9f66222a06df07c16633713ca7b725a7
Harrison Shoebridge <harrison@theshoebridges.com>
2017-08-02 21:05:05 -0700

    Add overlap explanation

--- src/games/overlap.js
+++ src/games/overlap.js
@@ -1,4 +1,14 @@
 class Overlap extends Characters {
+  constructor(id) {
+    super(id)
+
+    this.light = new PIXI.Graphics()
+    this.light.radius = 30
+    this.light.padding = 10
+
+    this.stage.addChild(this.light)
+  }
+
   update(dt) {
     if (this.input.up) {
       this.steven.y -= 50*dt
@@ -18,5 +28,20 @@ class Overlap extends Characters {
 
     let a = this.physics.toAABB(this.steven)
     let b = this.physics.toAABB(this.reina)
+
+    let g = this.light
+    g.clear()
+
+    if (this.physics.intersects(a, b)) {
+      g.beginFill(0x9EDD88)
+    } else {
+      g.beginFill(0xECA5B3)
+    }
+
+    let offset = this.light.radius + this.light.padding
+
+    g.drawCircle(offset, offset, this.light.radius)
+
+    g.endFill()
   }
 }
